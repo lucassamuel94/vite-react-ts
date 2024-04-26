8dd2ea76b68ae76e497272655ccb575035d5d777
Lucas Samuel <lucassamuel.dev@gmail.com>
2024-04-26 12:05:48 -0300

    rename tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,7 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from 'tailwindcss'
+
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}'
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       container: {
@@ -14,10 +12,9 @@ export default {
         }
       },
       fontFamily: {
-        'sans': 'Inter, ui-sans-serif, sans-serif'
+        sans: 'Inter, ui-sans-serif, sans-serif'
       }
     }
   },
   plugins: []
-}
-
+} satisfies Config
